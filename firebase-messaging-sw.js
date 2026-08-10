044b70672f6b545ee0c93a29910dfdfc0ea30911
taefstore01-ops <taefstore01@gmail.com>
2026-08-10 15:59:37 +0300

    Add files via upload

---
+++ firebase-messaging-sw.js
@@ -0,0 +1,13 @@
+importScripts('https://www.gstatic.com/firebasejs/10.13.1/firebase-app-compat.js');
+importScripts('https://www.gstatic.com/firebasejs/10.13.1/firebase-messaging-compat.js');
+
+firebase.initializeApp({
+  apiKey: 'AIzaSyBoXJ89z_0bYYbx8qfSLZ6hElcFxCMI8PM',
+  authDomain: 'madrasati-app-1bf3f.firebaseapp.com',
+  projectId: 'madrasati-app-1bf3f',
+  storageBucket: 'madrasati-app-1bf3f.firebasestorage.app',
+  messagingSenderId: '123283288104',
+  appId: '1:123283288104:web:e1af96c78a55feb3db051d',
+});
+
+firebase.messaging();
